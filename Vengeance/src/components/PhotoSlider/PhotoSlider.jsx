05a0ebb3d88f7e63a561371fbaac511875a1b734
The15thSin <ayushjalan1203@gmail.com>
2023-12-10 01:26:42 +0530

    updated photoslider

--- Vengeance/src/components/PhotoSlider/PhotoSlider.jsx
+++ Vengeance/src/components/PhotoSlider/PhotoSlider.jsx
@@ -1,49 +1,66 @@
 import React, { useState } from 'react'
 import './PhotoSlider.css'
+import { motion } from "framer-motion"
 
 function PhotoSlider() {
     const corousel = [
         {
             id: 0,
             title: "photo 1",
-            url: "https://picsum.photos/id/237/300/200"
+            url: "https://picsum.photos/600/400"
         },
         {
             id: 1,
             title: "photo 2",
-            url: "https://picsum.photos/id/238/300/200"
+            url: "https://picsum.photos/id/200/600/400"
         },
         {
             id: 2,
             title: "photo 3",
-            url: "https://picsum.photos/id/239/300/200"
+            url: "https://picsum.photos/id/100/600/400"
         }
     ]
 
     const [imgId, setImgId] = useState(0);
+    const [dir, setDir] = useState(0);
     let imglink = corousel[imgId].url;
 
-    const decImgId = () =>{
-        if (imgId==0){
-            setImgId(corousel.length-1);
+    const decImgId = () => {
+        setDir(-1);
+        if (imgId == 0) {
+            setImgId(corousel.length - 1);
         }
-        else{
-            setImgId(imgId-1);
+        else {
+            setImgId(imgId - 1);
         }
     }
-    const incImgId = () =>{
-        if (imgId==corousel.length-1){
+    const incImgId = () => {
+        setDir(1);
+        if (imgId == corousel.length - 1) {
             setImgId(0);
         }
-        else{
-            setImgId(imgId+1);
+        else {
+            setImgId(imgId + 1);
         }
     }
 
     return (
-        <div className = 'PhotoSlider'>
-            <div className = 'ps-img'>
-                <img src={imglink} alt="" />
+        <div className="PhotoSlider">
+            <div className='ps-img'>
+                <motion.img src={imglink} alt="img"
+                    initial={{  x: (dir>0)?800:-800,
+                                opacity:0,
+                                transition:"ease-in"
+                    }}
+                    animate={{  x:0,
+                                opacity:1, 
+                                transition:"ease-in"
+                    }}
+                    exit={{     x:(dir>0)?-800:800,
+                                opacity:0,
+                                transition:"ease-in"
+                    }}
+                    key={imglink}/>
             </div>
             <span>
                 <button className='ps-action-butt' onClick={decImgId}>&#10094;</button>
