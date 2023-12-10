63e4343ee13f8b64fe893998fc86a0b72b32c2a4
dona-murmu <109920996+coding-junction@users.noreply.github.com>
2023-12-10 14:48:06 +0530

    bgColor-#BF40BF created faq section

--- Vengeance/src/App.jsx
+++ Vengeance/src/App.jsx
@@ -1,11 +1,14 @@
-// import './App.css'
+import './App.css'
+import Faqs from "./components/Faqs/Faqs"
+import Socials from './components/Socials/Socials'
 
 function App() {
  
 
   return (
     <>
-      
+      <Faqs/>
+      <Socials/>
     </>
   )
 }
---
+++ Vengeance/src/components/Socials/Socials.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+// import Iframe from 'react-iframe'
+// export default function Socials() {
+//     <div className='youtube'>
+//          <iframe 
+//          width="560" height="315" src="https://www.youtube.com/embed/Fd5clV9SzRE?si=0qWugdN1XKdhCmh6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+//          </iframe>
+//     </div>
+   
+//   return (
+//     <div>
+//         <Socials/>
+//     </div>
+//   )
+// }
