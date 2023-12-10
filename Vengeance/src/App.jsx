1dbe5d0f24c28e883935fa29b29f8da0d107cdb8
The15thSin <ayushjalan1203@gmail.com>
2023-12-10 20:35:08 +0530

    added maps

--- Vengeance/src/App.jsx
+++ Vengeance/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
 import Login from './components/Login/Login';
+import Maps from './components/Maps/Maps'
 import PhotoSlider from './components/PhotoSlider/PhotoSlider';
 import Footer from "./components/Footer/Footer";
+import Nav from './components/Header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      
-
+      <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path = "/" element={
+            <>
+              <Login />
+              <PhotoSlider />
+            </>
+          } />
+          <Route path = "/maps" element={
+            <Maps />
+          }/>
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </>
   )
 }
