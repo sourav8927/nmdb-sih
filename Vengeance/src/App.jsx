8ecaf1913df7d28b6ec0e3178c127f3048e3da3d
Dipa-44 <dipaghosh0902@gmail.com>
2023-12-11 16:36:55 +0530

    About us pushed

--- Vengeance/src/App.jsx
+++ Vengeance/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css';
+import './App.css';;
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Faqs from './components/Faqs/Faqs';
 import Socials from './components/Socials/Socials';
 import Login from './components/Login/Login';
 import Maps from './components/Maps/Maps';
 import PhotoSlider from './components/PhotoSlider/PhotoSlider';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import ChooseUser from './components/ChooseUser/ChooseUser';
-
+import Footer from "./components/Footer/Footer";
+import Aboutus from "./components/Aboutus/Aboutus";
 
 function App() {
   return (
+    <>
+      <Aboutus />
+    </>
     <div className='app'>
       <BrowserRouter>
       <Header />
