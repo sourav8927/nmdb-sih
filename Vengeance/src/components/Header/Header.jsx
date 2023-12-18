cc03b2e71a66298f8d2e527e816dc9adb411a671
Tanay Pramanick <tanaypramanick41@gmail.com>
2023-12-18 18:06:28 +0530

    header v-4

--- Vengeance/src/components/Header/Header.jsx
+++ Vengeance/src/components/Header/Header.jsx
@@ -1,63 +1,56 @@
 import "./Header.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 // import LoginPng from "./img/login.png";
 
-function Header() {
-  return (
-    <header className="header">
-      <div className="mini-header">
-        <div className="mini-logo">
-          <img src="logo.png" alt="Logo" />
-        </div>
-        <div className="zoom-lang">
-          <div className="zoom">
-            <button>+A</button>
-            <button>A</button>
-            <button>-A</button>
-          </div>
-          <div className="lang">
-            <button className="bigbutton">Language</button>
-          </div>
-        </div>
-      </div>
-      <div className="main-header">
-        <div className="logo">
-          <img className="logo-img" src="img/logo.png" alt="Logo" />
-        </div>
-        <nav className="nav-links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <Link to="/maps">Dashboard</Link>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="search-login-user">
-        <div class="search-bar">
-        <input type="text" placeholder="Search..." />
-        <button type="button">Search</button>
-        </div>
-          <div className="login-icons">
-            <Link to="/login" className="login-icon">
-              <img src="img/login.png" alt="Login" />
-            </Link>
-            
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+function Header(){
+    return(
+        <>
+            <header className="header">
+                <div className="mini-header">
+                    <div className="mini-logo">
+                        <img src="logo.png" alt="Logo" />
+                    </div>
+                    <div className="zoom-lang">
+                        <div className="zoom">
+                            <button>+A</button>
+                            <button>A</button>
+                            <button>-A</button>
+                        </div>
+                        <div className="lang">
+                            <button className="bigbutton">Language</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="main-header">
+                    <div className="logo">
+                        <img className="logo-img" src="img/logo.png" alt="Logo" />
+                    </div>
+                    <nav className="nav-links">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><Link to="/maps">Dashboard</Link></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <div className="search-login-user">
+                <div className="search-bar">
+                <input type="text" placeholder="Search..." />
+                <button type="button">Search</button>
+                </div>
+                    <div className="login-icons">
+                        <Link to='LoginUser' className="login-icon"><img src="img/login.png" alt="Login"/></Link>
+                        {/* <a href="#" className="user-icon"><img src="user-icon.png" alt="User"/></a> */}
+                    </div>
+                </div>
+                </div>
+                
+                
+            </header>
+    </>
+    )
 }
 export default Header;
