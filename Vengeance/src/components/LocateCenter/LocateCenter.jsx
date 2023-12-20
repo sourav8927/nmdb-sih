4b5c6bf50e2e74baa5929b1f3f5c0e97e02530c4
Dipa-44 <dipaghosh0902@gmail.com>
2023-12-20 16:12:19 +0530

    table added in locate center

--- Vengeance/src/components/LocateCenter/LocateCenter.jsx
+++ Vengeance/src/components/LocateCenter/LocateCenter.jsx
@@ -5,7 +5,7 @@ import "../LocateCenter/LocateCenter.css";
 
 function LocateCenter() {
 
-  const [selectedState, setSelectedState] = useState();
+  const [selectedState, setSelectedState] = useState("Gujarat");
   const [selectedDistrict, setSelectedDistrict] = useState("select");
 
   let availableState = [""];
@@ -16,60 +16,57 @@ function LocateCenter() {
     availableState = json.states.find((s) => s.state === selectedState);
     console.log(availableState);
   }
-
-  function changeDistrict(d) {
-    setSelectedDistrict(d);
-  }
-
   return (
     <div>
       <center><h1>Locate center</h1></center>
       <div className="lc">
         <div className="lc-state">
           <p className='lc-p'>Select your state</p>
-          <select className='opt' value={selectedState} onChange={(e) => changeState(e.target.value)}>
-            {
-              json.states.map((value, key) => {
-                return (
-                  <option value={value.state} key={key}>
-                    {value.state}
-                  </option>
-                )
-              })
-            }
+          <select className='opt'>
+            <option></option>
           </select>
         </div>
 
         <div className="lc-state">
           <p className='lc-p'>Select your district</p>
-          <select className='opt' value={selectedDistrict} onChange={(e) => setSelectedDistrict(e.target.value)}>
-            {/* {
-              availableState.districts.map((value,key) => {
-                return (
-                  <option value={value.districts} key={key}>
-                      {value.districts}
-                  </option>
-                )
-              })
-            } */}
+          <select className='opt'>
+            <option></option>
           </select>
         </div>
       </div>
+      <center> <button>Search</button></center>
+        <table>
+    <thead>
+      <tr>
+        <th>Center Name</th>
+        <th>Address</th>
+        <th>Beneficiary Number</th>
+        <th>Recovered</th>
+        <th>View Deatils</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Center 1</td>
+        <td>123 Main Street, Cityville</td>
+        <td>100</td>
+        <td>29</td>
+        <td><button>View</button></td>
+      </tr>
+      <tr>
+        <td>Center 2</td>
+        <td>456 Oak Avenue, Townsville</td>
+        <td>300</td>
+        <td>220</td>
+        <td><button>View</button></td>
+      </tr>
+   
+    </tbody>
+  </table>
 
-      <div className="center-details">
-        <div className="center1">
-          <p className='lc-cm'>Center Name: </p>
-          <p className='lc-cm'>Address: </p>
-        </div>
-      </div>
 
-      <div className="center-details">
-        <div className="center1">
-          <p className='lc-cm'>Center Name: </p>
-          <p className='lc-cm'>Address: </p>
-        </div>
-      </div>
-
+    
     </div>
   )
 
