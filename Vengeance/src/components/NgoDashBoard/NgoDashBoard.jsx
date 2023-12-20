6f86b0f3aef0140f3743abbcd0327b2f64fe60c7
Tanay Pramanick <tanaypramanick41@gmail.com>
2023-12-20 08:39:56 +0530

    tanay1

--- Vengeance/src/components/NgoDashBoard/NgoDashBoard.jsx
+++ Vengeance/src/components/NgoDashBoard/NgoDashBoard.jsx
@@ -11,19 +11,19 @@ function NgoDashBoard(){
     <div className="dashlowerbody">
        <div className="outercontainer">
        <div className="container-c1">
-          <Link to="/NgoDashActions"><div className="eliment"><p>add</p> </div></Link>
-          <div className="eliment"><p>update</p> </div>
-          <div className="eliment"><p>delete</p> </div>
+          <Link to="/NgoDashActions"><div className="eliment"><button>add</button> </div></Link>
+          <div className="eliment"><button>update</button> </div>
+          <div className="eliment"><button>delete</button> </div>
        </div>
        <div className="container-c2">
-          <div className="eliment"><p>track</p> </div>
-          <div className="eliment"><p>event</p> </div>
-          <div className="eliment"><p>galary</p> </div>
+          <div className="eliment"><button>track</button> </div>
+          <div className="eliment"><button>event</button> </div>
+          <div className="eliment"><button>galary</button> </div>
        </div>
        <div className="container-c3">
-          <div className="eliment"><p>profile</p> </div>
-          <div className="eliment"><p>feedback</p> </div>
-          <div className="eliment"><p>3</p> </div>
+          <div className="eliment"><button>profile</button> </div>
+          <div className="eliment"><button>feedback</button> </div>
+          <div className="eliment"><button>under construction</button> </div>
        </div>
        </div>
     </div>
